fac030103e70187be65f998b184141a9114e7dce
Arvid Reinert <168847324+arvidreinert@users.noreply.github.com>
2025-04-28 06:21:10 +0200

    .

--- host.py
+++ host.py
@@ -13,17 +13,17 @@ clock = pygame.time.Clock()
 PORT = 9000 
 players_connected = {}
 last_action_to = ()
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(("0.0.0.0", PORT))
+sok = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sok.bind(("0.0.0.0", PORT))
 print(f"server: {hn}.local ({ip})")
 #to who, what
 actions_receveid = "False"
 message_count = 0
 message_history = []
 while True:
-    ready,_,_ = select.select([s],[],[],1)
+    ready,_,_, = select.select([sok],[],[],1)
     if ready:
-        data,addr = s.recvfrom(4096)
+        data,addr = sok.recvfrom(4096)
         data = pickle.loads(data)
         if data == "break":
             print("shutting down server")
@@ -32,10 +32,10 @@ while True:
         if data == "login":
             players_connected[addr[1]] = data
             print(players_connected)
-            s.sendto(pickle.dumps(f"logged in succesfull,{len(list(players_connected))}"), addr)
+            sok.sendto(pickle.dumps(f"logged in succesfull,{len(list(players_connected))}"), addr)
         if data == "req peer online":
             l = len(list(players_connected))
-            s.sendto(pickle.dumps(str(l)), addr)
+            sok.sendto(pickle.dumps(str(l)), addr)
 
         if "actio;" in data:
             print("received from:", addr)
@@ -46,14 +46,14 @@ while True:
 
         if data == "req:actio":
             if actions_receveid == "False":
-                s.sendto(pickle.dumps("False"), addr)
+                sok.sendto(pickle.dumps("False"), addr)
             else:
                 if last_action_to[1] == addr[1]:
-                    s.sendto(pickle.dumps("False"), addr)
+                    sok.sendto(pickle.dumps("False"), addr)
                 else:
                     print("sent to:",addr,"count:",message_count)
                     print(actions_receveid, "send")
-                    s.sendto(pickle.dumps(f"{actions_receveid}?{message_count}"), addr)
+                    sok.sendto(pickle.dumps(f"{actions_receveid}?{message_count}"), addr)
                     message_history.append(actions_receveid)
                     if len(message_history) >=151:
                         del message_history[0]
@@ -66,4 +66,4 @@ while True:
             str = ""
             for msg in message_history[int(t):int(e)]:
                 str += f"{msg}="
-            s.sendto(pickle.dumps(f"{str}"), addr)
+            sok.sendto(pickle.dumps(f"{str}"), addr)
